716ca378cc2ed1b0427157dd1a48b4df260fa382
Benjamin Fernandez <fernandez.benjamin@outlook.com>
2025-07-19 08:39:19 +0200

    Upgrade dependancies

---
+++ src/main_test.rs
@@ -0,0 +1,19 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{
+    bindings::xdp_action,
+    macros::xdp,
+    programs::XdpContext,
+};
+
+#[xdp]
+pub fn netshield_ebpf(_ctx: XdpContext) -> u32 {
+    // Simple test: always allow all traffic
+    xdp_action::XDP_PASS
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
